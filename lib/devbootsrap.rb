bb64f073b8c9abf8f9f19d8ca16dc6220d526e27
ratnakar <ratnakarrao_nyros@yahoo.com>
2014-03-27 12:30:27 +0530

    bootstrap integrated

--- lib/devbootsrap.rb
+++ lib/devbootsrap.rb
@@ -497,3 +497,20 @@ require 'devise/models'
 require 'devise/modules'
 require 'devise/rails'
 
+
+
+
+##### Bootstrap code 
+
+
+module Twitter
+  module Bootstrap
+    module Rails
+      require 'twitter/bootstrap/rails/engine' if defined?(Rails)
+    end
+  end
+end
+
+#require 'less-rails'
+require 'twitter/bootstrap/rails/bootstrap' if defined?(Rails)
+
